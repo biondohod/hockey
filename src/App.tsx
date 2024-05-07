969de8eeaf774e6abe3416c362636516db498e3c
biondohod <biondohod@gmail.com>
2024-05-07 19:44:05 +0500

    add auth markup

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
+import { Route, Routes } from "react-router-dom";
 import Main from "./_root/pages/Main/Main";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
+import Authorization from "./_root/pages/Autorization/Authorization";
 
 function App() {
   return (
     <>
       <Header />
       <main>
-        <Main />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="authorization" element={<Authorization />} />
+        </Routes>
       </main>
       <Footer />
     </>
